16d3273bcc7dcc3aa3797aba755119a13777bc04
vnoook <noook@yandex.ru>
2022-08-09 12:13:52 +0700

    #74 add @staticmethod time_count()

--- main.py
+++ main.py
@@ -411,6 +411,10 @@ class Window(PyQt5.QtWidgets.QMainWindow):
         #     print(f'{globals()["Recipient" + str(count_obj)].get_all_info()}')
         # print()
 
+        print(f'примерное время выполнения '
+              f'{self.time_count(RecipientData.count_Recipient, self.q_pocket, self.q_messages, self.send_delay)}'
+              f' секунд')
+
         # настройка прогресс-бара
         self.progressBarStat.setMaximum(RecipientData.count_Recipient)
         self.progressBarStat.setValue(0)
@@ -530,6 +534,24 @@ class Window(PyQt5.QtWidgets.QMainWindow):
             self.window_info.exec_()
 
             return f'{_ex}\nЭлектронное письмо не отправлено, проверьте логин-пароль!'
+
+    # функция расчёта примерного времени
+    @staticmethod
+    def time_count(letters_all=16, letters_pack=5, delay_letter=3, delay_pack=300):
+        q_full_pack = letters_all // letters_pack
+
+        if letters_all % letters_pack == 0:
+            time_pack = (q_full_pack - 1) * delay_pack
+            time_letters = (((letters_pack - 1) * delay_letter) * q_full_pack)
+            time_short_pack = 0
+        else:
+            time_pack = q_full_pack * delay_pack
+            time_letters = (((letters_pack - 1) * delay_letter) * q_full_pack)
+            time_short_pack = ((letters_all % letters_pack) - 1) * delay_letter
+
+        time_all = time_pack + time_letters + time_short_pack
+
+        return time_all
 
     # событие - нажатие на кнопку Выход
     @staticmethod
